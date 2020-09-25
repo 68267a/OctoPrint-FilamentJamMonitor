0c51a460b56a868f15b29dfd7b127c69b84156c1
Barkas <barkas@jen.lan>
2020-09-25 00:15:26 -0400

    hi ho, hi ho

--- octoprint_filamentjammonitor/__init__.py
+++ octoprint_filamentjammonitor/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-
+from pynput import mouse
+import time
 import octoprint.plugin
 
 class FilamentJamMonitorPlugin(octoprint.plugin.StartupPlugin,
@@ -10,7 +11,26 @@ class FilamentJamMonitorPlugin(octoprint.plugin.StartupPlugin,
                       octoprint.plugin.ProgressPlugin):
 
     def get_settings_defaults(self):
-        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+        return({
+            'enabled': True,
+            'monitor_method': 'mouse',  #alt: gpio
+            'mouse_leftclick': '',      #could probably do something here
+            'mouse_rightclick': '',     #do something when clicked?
+            'monitor_pin': '',          #not used if monitor_method is mouse
+            'poll_style': 'percent',    #alt: timer
+            'poll_interval': 2,         #every 2%
+            'poll_timer': 10,           #how long to monitor, in seconds
+            'movement_counter': 0,      #DO NOT MODIFY
+            'movement_threshold': 100,  #How much movement should we detect before it's "ok"
+            'movement_fail_count': 3,   #how many times should the check fail before abort?
+            'stop_print': True,         #should we stop the print?
+            'stop_type': 'stop',        #stop or pause
+            'notify': False,            #should we notify? (not implemented)
+            'notify_who': ''            #who should we notify?
+            'usage_enabled': False,     #how much filament was used
+            'usage_rate': '',           #the amount of filament per scroll? ADVANCED!!
+            'usage_log_location': ''    #where to log filament usage.
+        })
 
     def get_template_configs(self):
         return [dict(type="settings", custom_bindings=False)]
@@ -25,9 +45,60 @@ class FilamentJamMonitorPlugin(octoprint.plugin.StartupPlugin,
     def on_after_startup(self):
         self._logger.info("Filament Jam Monitor! (more: %s)" % self._settings.get(["url"]))
 
+    def filament_moved(x,y):
+        global self.movement_counter, self.movement_threshold
+        self.movement_counter += 1
+    
+    def monitor_button(x,y,button,pressed):
+        self._logger.info('Button '+button+' pressed')
+    def monitor_scroll(x,y,dx,dy):
+        self._logger.info('Scroll '.format(
+            'down' if dy <0 else 'up', (x,y)
+        ))
+
     #https://github.com/jneilliii/OctoPrint-Tasmota/blob/7b8fb53a62d73bad4d7e73012e2db7c738748685/octoprint_tasmota/__init__.py#L257-L261
     def on_print_progress(storage,path,progress):
-        readMousek
+        read_movement
+
+    def read_movement():
+        global self.movement_fail_count, 
+            self.movement_counter,
+            self.movement_threshold
+
+        listener = mouse.Listener(
+            on_move=filament_moved,
+            on_click=monitor_button,
+            on_scroll=monitor_scroll)
+        
+        try:
+            listener.start()
+            time.sleep(self.poll_timer)
+            listener.stop
+        except Exception as e:
+            self._logger.error(e)
+
+        movement = str.format(
+            self.movement_counter'/'self.movement_threshold
+        )
+
+        if self.movement_threshold != 0:
+            if self.movement_counter > self.movement_threshold:
+                self._logger.info("Reached movement threshold "+movement)
+            else:
+                self.movement_fail_count -= 1
+                if self.movement_fail_count > 0:
+                    self._logger.warn("FAILURE #" +self.movement_fail_count+" TO DETECT SUFFICIENT FILAMENT MOVEMENT "+movement)
+                else:
+                    raise RuntimeError("Did not reach movement threshold "+movement)
+                    if self.stop_print:
+                        self._logger.error("HOW DO I STOP THIS THING")                
+                    else:
+                        self._logger.warn("Not stopping")
+                        self.movement_threshold = 0 # we get it, shut up
+        elif self.movement_threshold == 0:
+            ignoreThis = "This is a placeholder that does nothing and isn't referenced anywhere"
+        else:
+            raise ValueError("This shouldn't happen. "+str(self.movement_threshold))
 
 __plugin_name__ = "Filament Jam Monitor"
 __plugin_pythoncompat__ = ">=2.7,<4"
--- octoprint_filamentjammonitor/readMouse.py
+++ octoprint_filamentjammonitor/readMouse.py
@@ -3,21 +3,19 @@ from pynput import mouse
 import time
 
 counter = 0
-limit = 100
+threshold = 100
+timer = 10
 
 def on_move(x, y):
     #print('Pointer moved to {0}'.format((x, y)))
-    global counter, limit
+    global counter, threshold
     counter+=1
-    print(counter)
-    if counter > limit:
-        return True
 
 def on_click(x, y, button, pressed):
     print('{0} at {1}'.format(
         'Pressed' if pressed else 'Released',
         (x, y)))
-    if not pressed:
+    if not pressed: #I think this will hurt later
         # Stop listener
         return False
 
@@ -26,14 +24,19 @@ def on_scroll(x, y, dx, dy):
         'down' if dy < 0 else 'up',
         (x, y)))
 
-print(counter)
-# ...or, in a non-blocking fashion:
 listener = mouse.Listener(
     on_move=on_move,
     on_click=on_click,
     on_scroll=on_scroll)
-listener.start()
-time.sleep(15)
-Listener.stop
 
-print("did not detect sufficient movement.")
+try:
+    listener.start()
+    time.sleep(timer)
+    listener.stop
+except Exception as e:
+    print(e)
+
+if counter > threshold:
+    print("reached threshold " + str(counter)+"/"+str(threshold))
+else:
+    raise RuntimeError("Did not reach threshold " + str(counter)+"/"+str(threshold))
